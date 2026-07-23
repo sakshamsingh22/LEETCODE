d2406fa37cf94897afdb56d2d89ee7455751d110
Saksham Singh <149508304+sakshamsingh22@users.noreply.github.com>
2026-07-24 04:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (14.76%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,9 +4,7 @@ public:
         int current =nums[0];
         int ans= nums[0];
         for(int i=1;i<nums.size();i++){
-            int v1=current+nums[i];
-            int v2=nums[i];
-            current=max(v1,v2);
+            current=max(nums[i],current+nums[i]);
             ans=max(ans,current);
         }
         return ans;
