96edbb90997bea231f8a395c9c8f8473f505d6bd
Saksham Singh <149508304+sakshamsingh22@users.noreply.github.com>
2026-08-06 23:08:52 +0530

    Time: 2492 ms (11.24%) | Memory: 34.3 MB (94.34%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-
-        unordered_map<int, int> prefixFreq;
-
-        prefixFreq[0] = 1;
-
-        int prefixSum = 0;
-        int answer = 0;
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            // Current Prefix Sum
-            prefixSum += nums[i];
-
-            // Required Previous Prefix Sum
-            int neededPrefix = prefixSum - k;
-
-            // Kitni baar neededPrefix mila?
-            int frequency = prefixFreq[neededPrefix];
-
-            // Utne subarrays mil gaye
-            answer += frequency;
-
-            // Current Prefix Sum store/update karo
-            prefixFreq[prefixSum]++;
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int sum =0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum==k){
+                    count++;
+                }
+            }
         }
-
-        return answer;
+       return count;
+        
     }
 };
