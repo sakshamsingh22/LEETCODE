e50174cb50832a78bf7620a5d3d69f433b5134dd
Saksham Singh <149508304+sakshamsingh22@users.noreply.github.com>
2026-09-08 23:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (65.32%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -5,7 +5,7 @@ public:
         int left =0;
         int right=n-1;
         int currentsum;
-        while(left<=right){
+        while(left<right){
             currentsum = numbers[left] + numbers[right];
             if(currentsum==target){
                 return{left+1,right+1};
