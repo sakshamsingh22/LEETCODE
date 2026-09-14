378b4de03a5f89eb165ca045f2908a69b84e71ed
Saksham Singh <149508304+sakshamsingh22@users.noreply.github.com>
2026-09-15 01:08:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.04%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int minending=nums[0];
-        int maxending=nums[0];
+        int n=nums.size();
+        int curr=nums[0];
+        int currmin=nums[0];
         int res=nums[0];
-        for(int i=1;i<nums.size();i++){
-         int v1=nums[i];
-         int v2 = minending*nums[i];
-         int v3= maxending*nums[i];
-        maxending=max(v1,max(v2,v3));
-        minending=min(v1,min(v2,v3));
-        res =max(res,max(maxending,minending));
+        for(int i=1;i<n;i++){
+        int v1=nums[i];
+        int v2= curr*nums[i];
+        int v3=currmin*nums[i];
+       
+            int min_ending=max(v1,max(v2,v3));
+            int max_ending=min(v1,min(v2,v3));
+            res=max(res,max(min_ending,max_ending));
+            curr =max_ending;
+            currmin=min_ending;
 
         }
-       
+
+
         return res;
-        
     }
 };
