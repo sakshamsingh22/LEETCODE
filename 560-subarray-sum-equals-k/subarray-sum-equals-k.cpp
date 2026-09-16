746ca45fe16c5575400d3bcc6ec8ed5dcc42dc8b
Saksham Singh <149508304+sakshamsingh22@users.noreply.github.com>
2026-09-17 00:20:20 +0530

    Time: 35 ms (85.80%) | Memory: 46.9 MB (28.40%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count =0;
-        int n= nums.size();
-        vector<int> prefixsum(n,0);
-        prefixsum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefixsum[i]=prefixsum[i-1]+nums[i];
-        }
-        unordered_map<int,int>m;
-        for(int j=0;j<n;j++){
-            if(prefixsum[j]==k){
-                count++;
-
-            }
-            int val=prefixsum[j]-k;
-            if(m.find(val)!=m.end()){
-                count+=m[val];
-            }
-            if(m.find(prefixsum[j])==m.end()){
-                m[prefixsum[j]]=0;
-            }
-            m[prefixsum[j]]++;
-        }
+    int n=nums.size();
+    int count=0;
+    vector<int>prefixsum(n,0);
+    prefixsum[0]=nums[0];
+    for(int i=1;i<n;i++){
+        prefixsum[i]=prefixsum[i-1]+nums[i];
+    }
+    unordered_map<int,int>mp;
+    for(int j=0;j<n;j++){
+    if(prefixsum[j]==k){
+        count++;
+    }
+    int val=prefixsum[j]-k;
+    if(mp.find(val)!=mp.end()){
+        count=count+mp[val];
+    }
+    if(mp.find(prefixsum[j])==mp.end()){
+         mp[prefixsum[j]]=0;
+    }
+     mp[prefixsum[j]]++;
+    }
         return count;
     }
 };
